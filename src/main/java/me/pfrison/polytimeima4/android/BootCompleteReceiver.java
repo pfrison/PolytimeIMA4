da47716e7007f9e6efed8ba9fbaa04f3ca829591
Pierre Frison <pierre.frison@polytech-lille.net>
2018-11-13 00:04:31 +0100

    Correction de bug spécifique à Android Oreo

--- src/main/java/me/pfrison/polytimeima4/android/BootCompleteReceiver.java
+++ src/main/java/me/pfrison/polytimeima4/android/BootCompleteReceiver.java
@@ -4,12 +4,16 @@ import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 public class BootCompleteReceiver extends BroadcastReceiver {
     @SuppressLint("UnsafeProtectedBroadcastReceiver") // protected via manifest
     @Override
     public void onReceive(Context context, Intent intent) {
         Intent serviceIntent = new Intent(context, NotificationService.class);
-        context.startService(serviceIntent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            context.startForegroundService(serviceIntent);
+        else
+            context.startService(serviceIntent);
     }
 }
